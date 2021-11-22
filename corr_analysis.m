c1b797e5e6c6b7f06d87bf6be1352255267998c6
Ilay P <90720820+ilaypinto@users.noreply.github.com>
2021-11-22 14:54:16 +0200

    Update corr_analysis.m

--- corr_analysis.m
+++ corr_analysis.m
@@ -7,19 +7,18 @@ best_feat_label = {};
 best_feat_feat = {};
 
 % Slice the data to features and labels
-label_vec = feat_label_mat(:, end);                                      % Separate the Labels from matrix
-feat_mat = feat_label_mat(:, 1:end - 1);                                 % Separate Features from labels
+label_vec = feat_label_mat(:, end);                                        % Separate the Labels from matrix
+feat_mat = feat_label_mat(:, 1:end - 1);                                   % Separate Features from labels
 
 % Compute correlation
 feat_label_corr = corr(feat_mat, label_vec, 'type', 'Spearman');           % Features-Labels correlation
 feat_feat_corr = corr(feat_mat, feat_mat, 'type', 'Spearman');             % Features-Features correlation
 feat_feat_corr_under_0_7 = sum(sum(abs(feat_feat_corr) < 0.7))/2;          % Counts number of correlation under 0.7
-% gplotmatrix(feat_mat, [], label_vec');                                     % gplot
 
 % extract the feature with highest feature label correlation
-best_feat_label{1} = max(abs(feat_label_corr));                             % value of corr
-best_feat_label{2} = find(feat_label_corr == max(abs(feat_label_corr)));    % index of feature in matrix
-best_feat_label{3} = feat_names(best_feat_label{2});                        % feature name
+best_feat_label{1} = max(abs(feat_label_corr));                            % value of corr
+best_feat_label{2} = find(feat_label_corr == max(abs(feat_label_corr)));   % index of feature in matrix
+best_feat_label{3} = feat_names(best_feat_label{2});                       % feature name
 
 % extract the features with lowest festure feature correlation
 [M,I] = min(abs(feat_feat_corr), [], 'all');
@@ -33,4 +32,9 @@ best_feat_feat{1} = M;                                                      % va
 best_feat_feat{2} = [index1, index2];                                       % index of features
 best_feat_feat{3}{1} = feat_names{index1};                                  % names of features
 best_feat_feat{3}{2} = feat_names{index2};
+
+%Graphs
+% figure(1); gplotmatrix(feat_mat, [], label_vec');                               % gplot
+% figure(2); heatmap(abs(feat_feat_corr));title('Spearman correlation - Heatmap') % correlation heatmap
+
 end
