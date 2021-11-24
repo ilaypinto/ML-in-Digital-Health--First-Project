691a3a5904ab547be6bfb824e7f5af1038c23ee5
Ilay P <90720820+ilaypinto@users.noreply.github.com>
2021-11-24 18:09:14 +0200

    Update corr_analysis.m

--- corr_analysis.m
+++ corr_analysis.m
@@ -1,10 +1,11 @@
-function [feat_feat_corr_under_0_7, feat_feat_corr, feat_label_corr, best_feat_feat,...
-    best_feat_label] = corr_analysis(feat_label_mat, feat_names)
+function [feat_feat_corr_num_under_0_7, feat_feat_corr, feat_label_corr, best_feat_feat,...
+    best_feat_label,features_to_remove] = corr_analysis(feat_label_mat, feat_names)
 % need to add description
 
 % define empty cells arrays
 best_feat_label = {};
 best_feat_feat = {};
+features_to_remove={};
 
 % Slice the data to features and labels
 label_vec = feat_label_mat(:, end);                                        % Separate the Labels from matrix
@@ -13,7 +14,7 @@ feat_mat = feat_label_mat(:, 1:end - 1);                                   % Sep
 % Compute correlation
 feat_label_corr = corr(feat_mat, label_vec, 'type', 'Spearman');           % Features-Labels correlation
 feat_feat_corr = corr(feat_mat, feat_mat, 'type', 'Spearman');             % Features-Features correlation
-feat_feat_corr_under_0_7 = sum(sum(abs(feat_feat_corr) < 0.7))/2;          % Counts number of correlation under 0.7
+feat_feat_corr_num_under_0_7 = sum(sum(abs(feat_feat_corr) < 0.7))/2;          % Counts number of correlation under 0.7
 
 % extract the feature with highest feature label correlation
 best_feat_label{1} = max(abs(feat_label_corr));                             % value of corr
@@ -38,4 +39,17 @@ best_feat_feat{3}{2} = feat_names{index2};
 % figure(1); gplotmatrix(feat_mat, [], label_vec');                               % gplot
 % figure(2); heatmap(abs(feat_feat_corr));title('Spearman correlation - Heatmap') % correlation heatmap
 
+%Names of features with over 0.7 feature-feature correlation
+
+indices=find(abs(feat_feat_corr) >= 0.7);
+indices_rows=ceil(indices./size(feat_feat_corr, 1));
+for i=1:length(indices)
+    if mod(indices(i), size(feat_feat_corr, 1)) == 0
+        indices_cols(i) = size(feat_feat_corr, 1);
+    else
+        indices_cols(i) = mod(indices(i), size(feat_feat_corr, 1));
+    end
+    features_to_remove{i}{1}=feat_names{indices_rows(i)};
+    features_to_remove{i}{2}=feat_names{indices_cols(i)};
+end
 end
