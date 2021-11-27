7ca6a1afdf20f425203bb6418aa1c8846bc0a9c0
tomerroditi <tomerroditi1@gmail.com>
2021-11-27 18:52:09 +0200

    changed corr computations

--- corr_analysis.m
+++ corr_analysis.m
@@ -1,62 +1,64 @@
-function [feat_feat_corr_num_under_0_7, feat_feat_corr, feat_label_corr, best_feat_feat,...
-    best_feat_label, features_removed_names, feature_removed_indices, new_feat_matrix, new_feat_names, highest_corr_under_thresh]...
+function [feat_feat_corr, weights_all, best_feat_label, features_removed_names,...
+    feature_removed_indices, new_feat_matrix, new_feat_names, highest_corr_under_thresh]...
     = corr_analysis(feat_label_mat, feat_names)
-% need to add description
+% this function computes correlations between features and relieff between fetures and labels.
+
+k = 10;     % num of neighbors for relieff
+
+% Slice the data to features and labels
+label_vec = feat_label_mat(:, end);         % Separate the Labels from matrix
+feat_mat = feat_label_mat(:, 1:end - 1);    % Separate Features from labels
 
 % define empty cells arrays
 best_feat_label = {};
-best_feat_feat = {};
+nan_feat_idx  = sum(isnan(feat_mat),1);
+nan_exmpl_idx = sum(isnan(feat_mat),2);
 
-% Slice the data to features and labels
-label_vec = feat_label_mat(:, end);                                        % Separate the Labels from matrix
-feat_mat = feat_label_mat(:, 1:end - 1);                                   % Separate Features from labels
 
-% Compute correlation
-feat_label_corr = corr(feat_mat, label_vec, 'type', 'Spearman');           % Features-Labels correlation
-feat_feat_corr = corr(feat_mat, feat_mat, 'type', 'Spearman');             % Features-Features correlation
-feat_feat_corr_num_under_0_7 = sum(sum(abs(feat_feat_corr) < 0.7))/2;          % Counts number of correlation under 0.7
+% Compute correlations
+[idx_relieff_all, weights_all] = relieff(feat_mat(nan_exmpl_idx == 0,:), label_vec(nan_exmpl_idx == 0,:), k);   % Features-Labels correlation fo all feat
+[idx_relieff_no_nan, weights_all_no_nan] = relieff(feat_mat(:,nan_feat_idx == 0), label_vec, k);                % Features-Labels correlation for feat without nans
+
+feat_feat_corr_nan = corr(feat_mat, 'type', 'Spearman', 'rows', 'all');                 % Features-Features correlation
+feat_feat_corr_all = corr(feat_mat, 'type', 'Spearman', 'rows', 'pairwise');            % Features-Features correlation
+feat_feat_corr = feat_feat_corr_nan;
+feat_feat_corr(isnan(feat_feat_corr)) = feat_feat_corr_all(isnan(feat_feat_corr));      % combine corr from feat with nan values
 
 % extract the feature with highest feature label correlation
-best_feat_label{1} = max(abs(feat_label_corr));                             % value of corr
-best_feat_label{2} = find(abs(feat_label_corr) == max(abs(feat_label_corr)));    % index of feature in matrix
-best_feat_label{3} = feat_names(best_feat_label{2});                        % feature name
-
-
-% extract the features with lowest festure feature correlation
-[M,I] = min(abs(feat_feat_corr), [], 'all');
-index2 = ceil(I/size(feat_feat_corr, 1));
-if mod(I, size(feat_feat_corr, 1)) == 0
-    index1 = size(feat_feat_corr, 1);
-else
-    index1 = mod(I, size(feat_feat_corr, 1));
-end
-best_feat_feat{1} = M;                                                      % value of corr
-best_feat_feat{2} = [index1, index2];                                       % index of features
-best_feat_feat{3}{1} = feat_names{index1};                                  % names of features
-best_feat_feat{3}{2} = feat_names{index2};
-
-%Graphs
-% figure(1); gplotmatrix(feat_mat, [], label_vec');                               % gplot
-% figure(2); heatmap(abs(feat_feat_corr));title('Spearman correlation - Heatmap') % correlation heatmap
+best_feat_label{1} = [weights_all(idx_relieff_all(1)), weights_all(idx_relieff_all(2))];    % value of relieff
+best_feat_label{2} = [idx_relieff_all(1), idx_relieff_all(2)];                           % index of feature in matrix
+best_feat_label{3} = feat_names([idx_relieff_all(1), idx_relieff_all(2)]);                  % feature name
 
 % find and remove features with over 0.7 feature-feature correlation
 indices = find(and(abs(feat_feat_corr) >= 0.7, abs(feat_feat_corr) ~= 1) );
 indices_cols = ceil(indices./size(feat_feat_corr, 1));
 indices_rows = mod(indices, size(feat_feat_corr, 1));
 indices_rows(indices_rows == 0) = size(feat_feat_corr, 1);
-features_removed_names = cell(1, length(indices));
 feature_removed_indices = zeros(1,length(indices));
 
 for i = 1:length(indices)
     feat_1 = indices_cols(i);
     feat_2 = indices_rows(i);
-    if feat_label_corr(feat_1) > feat_label_corr(feat_2)
+    if isnan(feat_feat_corr_nan(indices_rows(i),indices_cols(i)))
+        if strcmp(feat_names{feat_1}(1:4), 'baro') && strcmp(feat_names{feat_2}(1:4), 'baro')
+            if weights_all(feat_1) > weights_all(feat_2)
+                worst_feat = feat_2;
+            else
+                worst_feat = feat_1;
+            end
+        elseif strcmp(feat_names{feat_1}(1:4), 'baro')
+            worst_feat = feat_1;
+        else
+            worst_feat = feat_2;
+        end
+    elseif weights_all_no_nan(feat_1) > weights_all_no_nan(feat_2)
         worst_feat = feat_2;
-    else
+    elseif weights_all_no_nan(feat_1) < weights_all_no_nan(feat_2)
         worst_feat = feat_1;
     end
     feature_removed_indices(i) = worst_feat;
 end
+feature_removed_indices = [feature_removed_indices find(isnan(weights_all))];
 feature_removed_indices = unique(feature_removed_indices);
 features_removed_names = feat_names(feature_removed_indices);
 
@@ -75,5 +77,5 @@ highest_corr_under_thresh{1} = M;
 highest_corr_under_thresh{2} = I;            % use this value to check for correct names afterwards
 highest_corr_under_thresh{3} = feat_names{I_cols};
 highest_corr_under_thresh{4} = feat_names{I_rows};
-                                
+
 end
--- main.m
+++ main.m
@@ -13,14 +13,20 @@ flag_segm_MW  = 0;          % 1 - use the MW segmentation function, 0 - load sav
 flag_segm_ET  = 0;          % 1 - use the ET segmentation function, 0 - load saved segments
 flag_feat_MW  = 0;          % 1 - compute features for MW segments, 0 - load features
 flag_feat_ET  = 0;          % 1 - compute features for ET segments, 0 - load features
-flag_use_good_folders = 0;  % 1 - use good labeled folders,         0 - use all folders         
+flag_use_good_folders = 1;  % 1 - use good labeled folders,         0 - use all folders         
 
 % define some variables for later use
 label_time = 3;
 overlap = 90;
 segmentation = ['moving window'; 'event trigger'];
 labels_tags = [12 22 3 4 5 6 11 21 0];
-good_labels_folder = [];  % cherry pick folders with good labeled recordings
+good_labels_folder = [14 15	16	17	18	20	21	22	23	24	31	36	37	38	39	41	42	44 ...
+    45	46	47	49	50	51	52	53	54	55	57	58	59	60	73	74	75	76	77	78	79	80 ...
+    81	82	83	84	87	89	90	91	92	93	94	95	96	156	159	160	161	162	163	164	165	166 ...
+    167	168	173	174	175	176	178	181	182	183	184	185	186	187	188	189	190	191	192	205	206	208 ...
+    213	214	215	216	218	219	222	223	224	227	241	242	243	244	245	246	247	248	249	250	251	252 ...
+    253	254	256	257	258	259	260	262	265	266	267	268	269	270	271	272	273	274	275	276	277	279 ...
+    280	281	282	283	284	289	290	291	293	294	295	296	297	298	299	300];  % cherry pick folders with good labeled recordings
 
 %% order data in folders and load data from csv files
 % create a folder for each recording containing the sensors data and labels
@@ -41,8 +47,8 @@ end
 data = data_from_csv(folders, flag_data_csv, label_time);
 
 %% create train and test set for MW and ET segmentation
-folders_train = folders(1:round(0.75*length(folders)));         % try cherry pick with good labeled recordings
-folders_test = folders(round(0.75*length(folders)) + 1: end);   % try cherry pick with good labeled recordings
+folders_train = folders(1:round(0.8*length(folders)));         % try cherry pick with good labeled recordings
+folders_test = folders(round(0.8*length(folders)) + 1: end);   % try cherry pick with good labeled recordings
 
 MW_train_set = create_data_set(folders_train, data, segmentation(1,:), overlap, 'train', label_time, flag_segm_MW);
 MW_test_set = create_data_set(folders_test, data, segmentation(1,:), overlap, 'test', label_time, flag_segm_MW);
@@ -56,68 +62,72 @@ MW_train_feat = create_feat_set(MW_train_set, 'MW_train_feat', flag_feat_MW, lab
 MW_test_feat = create_feat_set(MW_test_set, 'MW_test_feat', flag_feat_MW , labels_tags);
 
 ET_train_feat = create_feat_set(ET_train_set, 'ET_train_feat', flag_feat_ET, labels_tags);
-ET_new_test_feat = create_feat_set(ET_test_set, 'ET_test_feat', flag_feat_ET, labels_tags);
+ET_test_feat = create_feat_set(ET_test_set, 'ET_test_feat', flag_feat_ET, labels_tags);
+
 
 %% feature selection process
 % ###### need to verify results of names and indices removed ######
 features_names = get_feat_names();          % create a cell array with the features names
-[ET_ff_corr_low, ET_ff_corr, ET_fl_corr, ET_best_ff, ET_best_fl, ET_features_removed,...
-    ET_feature_removed_indices, ET_new_train_feat, ET_new_features_names, ET_highest_corr_under_thresh] = corr_analysis(ET_train_feat, features_names);
+[ET_ff_corr, ET_fl_corr, ET_best_fl, ET_features_removed, ET_feature_removed_indices,...
+    ET_new_train_feat, ET_new_features_names, ET_highest_corr_under_thresh] = corr_analysis(ET_train_feat, features_names);
 
-[MW_ff_corr_low, MW_ff_corr, MW_fl_corr, MW_best_ff, MW_best_fl, MW_features_removed,...
-    MW_feature_removed_indices, MW_new_train_feat, MW_new_features_names, MW_highest_corr_under_thresh] = corr_analysis(MW_train_feat, features_names);
+[MW_ff_corr, MW_fl_corr, MW_best_fl, MW_features_removed, MW_feature_removed_indices,...
+    MW_new_train_feat, MW_new_features_names, MW_highest_corr_under_thresh] = corr_analysis(MW_train_feat, features_names);
 
+figure(2); heatmap(abs(corr(ET_new_train_feat(:,1:end-1), 'type', 'Spearman', 'rows', 'pairwise')));title('Spearman correlation - Heatmap (event trigger)') % correlation heatmap
+%% create new test features
 ET_vec = ones(1,size(ET_train_feat, 2));
 ET_vec(1,ET_feature_removed_indices) = 0;
 
 MW_vec = ones(1,size(MW_train_feat, 2));
-MW_vec(1,ET_feature_removed_indices) = 0;
+MW_vec(1,MW_feature_removed_indices) = 0;
 
-ET_new_test_feat = ET_new_test_feat(:, ET_vec == 1);
-MW_new_test_feat = MW_new_test_feat(:, MW_vec == 1);
-% compute CFS on selected features
-% event trigger
-[~, I_1_ET] = maxk(ET_fl_corr,14);
-[~, I_2_ET] = mink(ET_ff_corr(I_1_ET,:), 1, 2);
-I_2_ET = reshape(I_2_ET,1,numel(I_2_ET));
-I_ET = unique([I_1_ET' I_2_ET]);
-cfs_ET = calculate_CFS(ET_fl_corr,ET_ff_corr,I_ET);
-names_ET = features_names(I_ET);
-
-% moving window
-[~, I_1_MW] = maxk(MW_fl_corr,14);
-[~, I_2_MW] = mink(MW_ff_corr(I_1_MW,:), 10, 2);
-I_2_MW = reshape(I_2_MW,1,numel(I_2_MW));
-I_MW = unique([I_1_MW' I_2_MW]);
-cfs_MW = calculate_CFS(MW_fl_corr,MW_ff_corr,I_MW);
-names_MW = features_names(I_MW);
-
-disp('feat for MW')
-for i = 1:length(names_MW)
-    disp(names_MW{i})
-end
-disp([newline 'feat for ET'])
-for i = 1:length(names_ET)
-    disp(names_ET{i})
-end
-
-% compute relief - ###### need to find a solution for NaN #######
-% k = 10;
-% [relief_idx_MW, relief_weights_MW] = relieff(features_MW(:,1:end-1),features_MW(:,1:end-1),k);
-% [relief_idx_ET, relief_weights_ET] = relieff(features_ET(:,1:end-1),features_ET(:,1:end-1),k);
+ET_new_test_feat = ET_test_feat(:, ET_vec == 1);
+MW_new_test_feat = MW_test_feat(:, MW_vec == 1);
+% %% compute CFS on selected features
+% % event trigger
+% [~, I_1_ET] = maxk(ET_fl_corr,14);
+% [~, I_2_ET] = mink(ET_ff_corr(I_1_ET,:), 1, 2);
+% I_2_ET = reshape(I_2_ET,1,numel(I_2_ET));
+% I_ET = unique([I_1_ET' I_2_ET]);
+% cfs_ET = calculate_CFS(ET_fl_corr,ET_ff_corr,I_ET);
+% names_ET = features_names(I_ET);
+% 
+% % moving window
+% [~, I_1_MW] = maxk(MW_fl_corr,14);
+% [~, I_2_MW] = mink(MW_ff_corr(I_1_MW,:), 10, 2);
+% I_2_MW = reshape(I_2_MW,1,numel(I_2_MW));
+% I_MW = unique([I_1_MW' I_2_MW]);
+% cfs_MW = calculate_CFS(MW_fl_corr,MW_ff_corr,I_MW);
+% names_MW = features_names(I_MW);
+% 
+% disp('feat for MW')
+% for i = 1:length(names_MW)
+%     disp(names_MW{i})
+% end
+% disp([newline 'feat for ET'])
+% for i = 1:length(names_ET)
+%     disp(names_ET{i})
+% end
 
 %%
+options = statset('Display', 'iter', 'UseParallel', true);
 template = templateTree('MaxNumSplits', 20);
 classificationEnsemble = fitcensemble(ET_train_feat(:, 1:end - 1), ET_train_feat(:, end), 'Method', 'AdaBoostM2', 'NumLearningCycles', 30, 'Learners', template, 'ClassNames', [0; 3; 4; 5; 6; 11; 12; 21; 22]);
 
 %SFS- using loss function as criteria
 fun = @(Xtrain,Ytrain,Xtest,Ytest)loss(fitcensemble(Xtrain, Ytrain, 'Method', 'AdaBoostM2', 'Learners', template), Xtest, Ytest);
-[Indx_MW, history_MW] = sequentialfs(fun, MW_new_train_feat(:,1:5), MW_new_train_feat(:,end));
-[Indx_ET, history_ET] = sequentialfs(fun, ET_new_train_feat(:,1:5), ET_new_train_feat(:,end));
+[Indx_MW, history_MW] = sequentialfs(fun, MW_new_train_feat(:,1:end-1), MW_new_train_feat(:,end), 'options', options);
+%%
+[Indx_ET, history_ET] = sequentialfs(fun, ET_new_train_feat(:,1:end-1), ET_new_train_feat(:,end), 'options', options);
 
 
-
-
+%%
+figure(2); heatmap(abs(corr(ET_new_train_feat(:,1:end-1), 'type', 'Spearman', 'rows', 'pairwise'))); % correlation heatmap
+title('Spearman correlation - Heatmap (event trigger)');
+figure(3); heatmap(abs(corr(MW_new_train_feat(:,1:end-1), MW_new_train_feat(:,1:end-1), 'type', 'Spearman'))); % correlation heatmap
+title('Spearman correlation - Heatmap (moving window)');
+figure(1); gplotmatrix(feat_mat, [], label_vec');                               % gplot
 
 
 
@@ -129,20 +139,27 @@ fun = @(Xtrain,Ytrain,Xtest,Ytest)loss(fitcensemble(Xtrain, Ytrain, 'Method', 'A
 
 %% this are some plots to check the segmentation and label process is done as we wish - surprise surprise its NOT... :( 
 % the problem is not in our code (our files are being labeled and segmented very accurately) but in the label process 
-% done by some other groups.
+% done by some other groups, so we are exluding the bad labeled recordings.
 
-indx = 277;
-data = extract_data(int2str(indx), label_time);
-baro_1 = data.baro;
-gyro_1 = data.gyro;
-acc_1 = data.acc;
-figure(1);
-plot((1:length(baro_1(1,:))),baro_1(1,:)); hold on; plot(find(baro_1(2,:) ~= 0), baro_1(1, find(baro_1(2,:) ~= 0)) + 1.01*10^5,'b.' ); hold off;
-figure(2);
-plot((1:length(gyro_1(1,:))),gyro_1(1:3,:)); hold on; plot(find(gyro_1(4,:) ~= 0), gyro_1(1:3, find(gyro_1(4,:) ~= 0)),'b.' ); hold off;
-figure(3);
-plot((1:length(acc_1(1,:))),acc_1(1:3,:)); hold on; plot(find(acc_1(4,:) ~= 0), acc_1(1:3, find(acc_1(4,:) ~= 0)),'b.' ); hold off;
-
+% good_labels_folder = [];
+%
+% indx = folders;
+% for i = folders
+%     temp_data = data{i};
+%     baro_1 = temp_data.baro;
+%     gyro_1 = temp_data.gyro;
+%     acc_1 = temp_data.acc;
+% %     figure(1);
+% %     plot((1:length(baro_1(1,:))),baro_1(1,:)); hold on; plot(find(baro_1(2,:) ~= 0), baro_1(1, find(baro_1(2,:) ~= 0)) + 1.01*10^5,'b.' ); hold off;
+%     figure(2);
+%     plot((1:length(gyro_1(1,:))),gyro_1(1:3,:)); hold on; plot(find(gyro_1(4,:) ~= 0), gyro_1(1:3, find(gyro_1(4,:) ~= 0)),'b.' ); hold off;
+% %     figure(3);
+% %     plot((1:length(acc_1(1,:))),acc_1(1:3,:)); hold on; plot(find(acc_1(4,:) ~= 0), acc_1(1:3, find(acc_1(4,:) ~= 0)),'b.' ); hold off;
+%     store = input('good: ');
+%     if store == 1
+%         good_labels_folder(end + 1) = i;
+%     end
+% end
 % %%
 % for i = 1:9
 %     for j = 1:size(segments_ET(i).gyro,3)
